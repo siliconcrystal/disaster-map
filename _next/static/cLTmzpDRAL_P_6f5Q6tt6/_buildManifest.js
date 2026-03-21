f89cbe508413b3b8455d21b94e9d19227e827552
siliconcrystal <siliconcrystal@gmail.com>
2026-03-21 16:22:42 +0800

    Updates

--- _next/static/cLTmzpDRAL_P_6f5Q6tt6/_buildManifest.js
+++ _next/static/cLTmzpDRAL_P_6f5Q6tt6/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/disaster-map//_next/:path+",
+        "destination": "/disaster-map/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
