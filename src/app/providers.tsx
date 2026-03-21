007977d37ae92003d867ed5b2752e41b2fb431cc
siliconcrystal <siliconcrystal@gmail.com>
2026-03-21 19:15:28 +0800

    fix script bug for turbo in provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -18,7 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
       <TaskFullDetailModal />
       <TaskCreateModal />
